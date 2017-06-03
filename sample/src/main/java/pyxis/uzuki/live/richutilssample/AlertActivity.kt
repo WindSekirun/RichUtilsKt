b7ad862bb3994e8564bb65d6b71963afc66716e8
WindSekirun <h.mina.moe@gmail.com>
2017-06-03 20:53:24 +0900

    make sample runnable

--- sample/src/main/java/pyxis/uzuki/live/richutilssample/AlertActivity.kt
+++ sample/src/main/java/pyxis/uzuki/live/richutilssample/AlertActivity.kt
@@ -3,28 +3,40 @@ package pyxis.uzuki.live.richutilssample
 import android.content.DialogInterface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_alert.*
 import pyxis.uzuki.live.richutilskt.*
 
 class AlertActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alert)
 
-        alert(message = "Hello, world!")
+        alert.setOnClickListener({alert(message = "Hello, world!")})
 
-        confirm(message = "Hello, world!", callback = {
-            toast(message = "click confirm button")
+        selector.setOnClickListener({
+            val items:ArrayList<String> = ArrayList()
+            (1..5).mapTo(items) { it.toString() }
+
+            selector(items = items, callback = { _: DialogInterface, i: Int ->
+                toast(message = "click item -> $i")
+            })
         })
 
-        val items:ArrayList<String> = ArrayList()
-        (1..5).mapTo(items) { it.toString() }
-
-        selector(items = items, callback = { _: DialogInterface, i: Int ->
-            toast(message = "click item -> $i")
+        confirm.setOnClickListener({
+            confirm(message = "Hello, world!", callback = {
+                toast(message = "click confirm button")
+            })
         })
 
-        val progress = progress(message = "Loading...")
-        progress.dismiss()
+        progress.setOnClickListener({
+            val progress = progress(message = "Loading...")
+
+            runDelayed(10000, {
+                progress.dismiss()
+            })
+
+        })
     }
 }
 
--- sample/src/main/java/pyxis/uzuki/live/richutilssample/MainActivity.kt
+++ sample/src/main/java/pyxis/uzuki/live/richutilssample/MainActivity.kt
@@ -2,12 +2,18 @@ package pyxis.uzuki.live.richutilssample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        alert.setOnClickListener({ startActivity<AlertActivity>() })
+        bitmap.setOnClickListener({ startActivity<BitmapActivity>() })
+        date.setOnClickListener({ startActivity<DateActivity>() })
     }
 }
 
