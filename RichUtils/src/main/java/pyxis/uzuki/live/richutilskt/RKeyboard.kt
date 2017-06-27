2ba7bcc32f69fa487f554fe0a441c45c51c33d95
Pyxis <pyxis@uzuki.live>
2017-06-27 15:30:19 +0900

    add Dialog.hideKeyboard

--- RichUtils/src/main/java/pyxis/uzuki/live/richutilskt/RKeyboard.kt
+++ RichUtils/src/main/java/pyxis/uzuki/live/richutilskt/RKeyboard.kt
@@ -4,6 +4,7 @@
 package pyxis.uzuki.live.richutilskt
 
 import android.app.Activity
+import android.app.Dialog
 import android.content.Context
 import android.view.inputmethod.InputMethodManager
 
@@ -14,6 +15,17 @@ fun Activity.hideKeyboard() {
     val view = this.currentFocus
     if (view != null) {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
+
+/**
+ * hide keyboard
+ */
+fun Dialog.hideKeyboard() {
+    val view = this.currentFocus
+    if (view != null) {
+        val imm = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
