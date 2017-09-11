167b27cab3b68f4388288682f32c2cba081eba53
WindSekirun <h.mina.moe@gmail.com>
2017-09-11 21:45:45 +0900

    enhance RKeyboard
    null-check with .let

--- RichUtils/src/main/java/pyxis/uzuki/live/richutilskt/utils/RKeyboard.kt
+++ RichUtils/src/main/java/pyxis/uzuki/live/richutilskt/utils/RKeyboard.kt
@@ -12,10 +12,9 @@ import android.view.inputmethod.InputMethodManager
  * hide keyboard
  */
 fun Activity.hideKeyboard() {
-    val view = this.currentFocus
-    if (view != null) {
+    this.currentFocus?.let {
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
+        imm.hideSoftInputFromWindow(it.windowToken, 0)
     }
 }
 
@@ -23,9 +22,8 @@ fun Activity.hideKeyboard() {
  * hide keyboard
  */
 fun Dialog.hideKeyboard() {
-    val view = this.currentFocus
-    if (view != null) {
-        val imm = this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    this.currentFocus?.let {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(it.windowToken, 0)
     }
 }
