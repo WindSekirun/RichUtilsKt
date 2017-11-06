967b817779e26129c4eaff1fbfd08b7382d61e79
WindSekirun <h.mina.moe@gmail.com>
2017-11-06 21:54:32 +0900

    implement ContactList

---
+++ demo/src/main/java/pyxis/uzuki/live/richutilskt/demo/set/ContactSet.kt
@@ -0,0 +1,24 @@
+package pyxis.uzuki.live.richutilskt.demo.set
+
+import android.content.Context
+import pyxis.uzuki.live.richutilskt.demo.R
+import pyxis.uzuki.live.richutilskt.demo.item.CategoryItem
+import pyxis.uzuki.live.richutilskt.demo.item.ExecuteItem
+import pyxis.uzuki.live.richutilskt.demo.item.generateExecuteItem
+
+/**
+ * Created by pyxis on 06/11/2017.
+ */
+
+fun Context.getContactSet() : ArrayList<ExecuteItem> {
+    val list = arrayListOf<ExecuteItem>()
+
+    val getContactsList = generateExecuteItem(CategoryItem.CONTACT, "getContactsList",
+            getString(R.string.contact_message_getcontactslist),
+            "getContactsList()",
+            "RichUtils.getContactsList(ContactActivity.this);")
+
+    list.add(getContactsList)
+
+    return list
+}
