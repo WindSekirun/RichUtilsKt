65f7e6643df01a6a54d23854bc673dbb74f8031a
Pyxis <pyxis@uzuki.live>
2017-12-18 18:01:31 +0900

    implement anyContainsString, anyMatchRegex

--- RichUtils/src/main/java/pyxis/uzuki/live/richutilskt/utils/RText.kt
+++ RichUtils/src/main/java/pyxis/uzuki/live/richutilskt/utils/RText.kt
@@ -14,3 +14,23 @@ import android.text.TextUtils
  * return string empty state
  */
 fun String.isEmpty() = TextUtils.isEmpty(this)
+
+/**
+ * Returns `true` if at least one element matches condition of contains string
+ */
+fun anyContainsString(inputStr: String, items: Array<String>) = items.any { inputStr.contains(it, false) }
+
+/**
+ * Returns `true` if at least one element matches condition of contains string
+ */
+fun anyContainsString(inputStr: String, items: List<String>) = items.any { inputStr.contains(it, false) }
+
+/**
+ * Returns `true` if at least one element matches condition of matches regex
+ */
+fun anyMatchRegex(regex: String, items: Array<String>) = items.any { it.contains(regex) }
+
+/**
+ * Returns `true` if at least one element matches condition of matches regex
+ */
+fun anyMatchRegex(regex: String, items: List<String>) = items.any { it.contains(regex) }
