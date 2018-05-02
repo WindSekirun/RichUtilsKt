ac785a88cf9d274825eb65ed3ad8aaebd99bdf96
pyxis <pyxis@uzuki.live>
2018-05-02 12:53:49 +0900

    fixes #47

--- rpickimagedemo/src/main/java/pyxis/uzuki/live/rpickimagedemo/MainActivity.kt
+++ rpickimagedemo/src/main/java/pyxis/uzuki/live/rpickimagedemo/MainActivity.kt
@@ -1,21 +1,22 @@
 package pyxis.uzuki.live.rpickimagedemo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 import pyxis.uzuki.live.richutilskt.utils.RPickMedia
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnSelect.setOnClickListener {
             RPickMedia.instance.pickFromGallery(this, { code, path ->
-                Log.d(MainActivity::class.java.simpleName, "code: %s, path: %s".format(code, path))
+                txtPath.text = "code: $code, path: $path"
                 Glide.with(this).load(path).into(imgPicture)
             })
         }
