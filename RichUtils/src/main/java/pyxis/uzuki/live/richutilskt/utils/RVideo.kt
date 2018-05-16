07dbf3e68b3aeea8d77e3edd499790b5f08e9628
pyxis <pyxis@uzuki.live>
2018-05-16 13:05:00 +0900

    implement RVideo.getVideoFileId, getVideoThumbsPath, getVideoDuration

--- RichUtils/src/main/java/pyxis/uzuki/live/richutilskt/utils/RVideo.kt
+++ RichUtils/src/main/java/pyxis/uzuki/live/richutilskt/utils/RVideo.kt
@@ -3,7 +3,10 @@
 
 package pyxis.uzuki.live.richutilskt.utils
 
+import android.content.Context
+import android.database.Cursor
 import android.media.MediaMetadataRetriever
+import android.provider.MediaStore
 
 /**
  * get Width of Video which given String
@@ -29,4 +32,59 @@ fun String.getVideoHeight(): Int {
     val height = Integer.valueOf(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT))
     retriever.release()
     return height
+}
+
+/**
+ * get id from MediaStore.Video with given [videoPath]
+ */
+fun Context.getVideoFileId(videoPath: String): String {
+    val projection = arrayOf(MediaStore.Video.Media._ID)
+    val where = MediaStore.Video.Media.DATA + "=?"
+    val cursor = queryVideo(videoPath, projection, where)
+
+    return cursor?.use { it.getString(0) } ?: ""
+}
+
+/**
+ * get Path of Thumbnail from MediaStore.Video with given [videoId]
+ *
+ * you can get videoId from Context.getVideoFileId()
+ *
+ * @param context Context object
+ * @param videoId Video ID
+ */
+fun Context.getVideoThumbsPath(videoId: String): String {
+    val projection = arrayOf(MediaStore.Video.Thumbnails.DATA)
+    val where = MediaStore.Video.Thumbnails.VIDEO_ID + "=?"
+    val cursor = queryVideo(videoId, projection, where)
+
+    return cursor?.use { it.getString(0) } ?: ""
+}
+
+/**
+ * get Duration of Video from MediaStore.Video with given [videoId]
+ *
+ * you can get videoId from Context.getVideoFileId()
+ *
+ * @param context Context object
+ * @param videoId Video ID
+ */
+fun Context.getVideoDuration(videoId: String): Long {
+    val projection = arrayOf(MediaStore.Video.VideoColumns.DURATION)
+    val where = MediaStore.Video.VideoColumns._ID + "=?"
+    val cursor = queryVideo(videoId, projection, where)
+
+    return cursor?.use { it.getLong(0) } ?: 0
+}
+
+private fun Context.queryVideo(args: String, projection: Array<String>, where: String): Cursor? {
+    val uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+    val whereArgs = arrayOf(args)
+    val cursor = contentResolver.query(uri, projection, where, whereArgs, null)
+
+    if (cursor == null || !cursor.moveToFirst()) {
+        return null
+    }
+
+    return cursor
 }
